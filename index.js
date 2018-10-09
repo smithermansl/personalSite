edd030f8a2563a8564ddfff927ac01606cae005b
Shelby Smitherman <smithermansl@gmail.com>
2018-10-09 15:16:07 -0400

    modified section order

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ $(document).ready(() => {
   
   let upArrow = $('#backToTop')
 
-  upArrow.hide()
-  let arrowWillShow = $('#background').offset().top
+  upArrow.hide().addClass('fixed')
+  let backgroundSectionTop = $('#background').offset().top
 
   $(window).scroll(() => {
-    if($(window).scrollTop() > arrowWillShow) {
+    if($(window).scrollTop() >= backgroundSectionTop) {
       upArrow.show()
     }
 
-    if($(window).scrollTop() < arrowWillShow) {
+    if($(window).scrollTop() < backgroundSectionTop) {
       upArrow.hide()
+    }
+
+    let footerSectionTop = $('footer').offset().top
+  
+    if($(window).scrollTop() + $(window).height() >= footerSectionTop) {
+      upArrow.removeClass('fixed').addClass('relative')
+    }
+
+    if($(window).scrollTop() + $(window).height() < footerSectionTop) {
+      upArrow.removeClass('relative').addClass('fixed')
     }
   })
 
   upArrow.click(() => {
     $('html, body').animate({ scrollTop: 0}, 300)
-
   })
 
 })
