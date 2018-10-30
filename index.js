5e7198f705922b01d2d63447a336628ac4130409
Shelby Smitherman <smithermansl@gmail.com>
2018-10-30 12:53:36 -0400

    POST to /contact success

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const path = require('path')
 const PORT = 8888
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 if (process.env.NODE_ENV !== 'production') require('./secrets')
 
@@ -19,19 +20,21 @@ const mailer = require('nodemailer').createTransport({
 app.use(express.static(path.join(__dirname, './public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(morgan('dev'))
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
 
 app.post('/contact', (req, res) => {
-  // console.log('the req.body', req.body)
-  // res.status(200).send('hi')
+  let newBody = `Sender Name: ${req.body.name}, 
+  Sender email: ${req.body.email}, 
+  Message: ${req.body.message}`
+
   mailer.sendMail({
-    from: req.body.email,
     to: [process.env.GMAIL_ADDRESS],
     subject: req.body.subject || '[No Subject]',
-    html: req.body.message || '[No Message]'
+    html: newBody || '[No Message]'
   }, (err, info) => {
     if (err) return res.status(500).send(err)
     res.json({success: true})
