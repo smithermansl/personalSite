088dc9354302620c2b2e6c195d8192b00345fe4b
Shelby Smitherman <smithermansl@gmail.com>
2018-11-28 16:24:17 -0500

    update to formspree

--- index.js
+++ index.js
@@ -7,16 +7,6 @@ const PORT = process.env.PORT || 8888
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
-if (process.env.NODE_ENV !== 'production') require('./secrets')
-
-const mailer = require('nodemailer').createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.LOCATION,
-    pass: process.env.USER_SECRET
-  }
-})
-
 app.use(express.static(path.join(__dirname, './public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -24,21 +14,6 @@ app.use(morgan('dev'))
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
-})
-
-app.post('/contact', (req, res) => {
-  let newBody = `Sender Name: ${req.body.name}, 
-  Sender email: ${req.body.email}, 
-  Message: ${req.body.message}`
-
-  mailer.sendMail({
-    to: [process.env.LOCATION],
-    subject: req.body.subject || '[No Subject]',
-    html: newBody || '[No Message]'
-  }, (err, info) => {
-    if (err) return res.status(500).send(err)
-    res.json({success: true})
-  })
 })
 
 module.exports = app
--- public/submitForm.js
+++ public/submitForm.js
@@ -1,12 +1,26 @@
-$(document).on('submit', '#emailForm', () => {
-  $.post('/contact', $('#emailForm').serialize(), (result) => {
-    if (result.success) {
+let emailForm = $('#emailForm')
+
+emailForm.submit(e => {
+  e.preventDefault()
+
+  let children = Array.from(emailForm[0].children),
+  name = children[0].value,
+  email = children[1].value,
+  message = children[2].value
+
+  $.ajax({
+    url: '//formspree.io/smithermansl@gmail.com',
+    method: 'POST',
+    data: { name, email, message },
+    dataType: 'json',
+    success: () => {
       $('#initial_contact').addClass('hidden')
       $('#email_success').addClass('visible')
-    } else {
+    },
+    error: err => {
+      console.log(err)
       $('#initial_contact').addClass('hidden')
       $('#email_failure').addClass('visible')
     }
   })
-  return false
 })
